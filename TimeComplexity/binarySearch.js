7b750ebeadb0efb894e17e2f32978148792667f8
lazycoderlee <kbasha968@gmail.com>
2025-07-30 17:22:33 +0530

    func: binarySearch

--- TimeComplexity/binarySearch.js
+++ TimeComplexity/binarySearch.js
@@ -23,3 +23,28 @@ function binarySearch(arr, target) {
   // Step 5: Target not found
   return -1;
 }
+
+function binarySearch(nums, target) {
+  let left = 0; // Initialize left pointer
+  let right = nums.length - 1; // Initialize right pointer
+
+  while (right >= left) {
+    // Continue while the search space is valid
+    let middle = Math.floor(left + right) / 2; // Calculate the middle index
+    if (target === nums[middle]) {
+      // If the target is found
+      return middle;
+    } else if (target < nums[middle]) {
+      // If the target is less than the middle element
+      // Adjust the right pointer to search in the left half
+      right = middle - 1;
+    } else {
+      // If the target is greater than the middle element
+      // Adjust the left pointer to search in the right half
+      left = middle + 1;
+    }
+  }
+  return -1;
+}
+
+console.log(binarySearch([-1, 2, 5, 6, 10, 12, 15], 15));
